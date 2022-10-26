89547bb67afcdef86e0b3da54bb11d72641baa08
gustavenoksson <gustav.enoksson@live.se>
2022-10-26 12:14:49 +0200

    initial commit, added files for project

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<?= 'The Elephpent in the Room'; ?>
+<?php
+
+require __DIR__ . "/php/header.php";
+
+?>
+
+<body>
+    <header>
+        <h1>Adventure</h1>
+    </header>
+
+    <main>
+        <section></section>
+    </main>
+
+</body>
+
+</html>
